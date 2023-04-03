3603ffe5e1724a1929f7e427798a30190c1472ca
xDamneDx <xdamnedx@gmail.com>
2023-04-03 11:51:02 +0300

    add router & basic routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,25 @@
+import { BrowserRouter, Routes, Route, NavLink } from "react-router-dom";
+
+// Pages:
+import Home from "./pages/Home";
+import About from "./pages/About";
+
 export default function App() {
-  return <div>App</div>;
+  return (
+    <BrowserRouter>
+      <header>
+        <nav>
+          <h1>JobaRouter</h1>
+          <NavLink to="/">Home</NavLink>
+          <NavLink to="about">About</NavLink>
+        </nav>
+      </header>
+      <main>
+        <Routes>
+          <Route index element={<Home />} />
+          <Route path="about" element={<About />} />
+        </Routes>
+      </main>
+    </BrowserRouter>
+  );
 }
---
+++ src/pages/Home.jsx
@@ -0,0 +1,27 @@
+export default function Home() {
+  return (
+    <div className="home">
+      <h2>Home</h2>
+      <p>
+        Run in circles. Caticus cuteicus roll on the floor purring your whiskers
+        off is good you understand your place in my world meowzer purr prance
+        along on top of the garden fence, annoy the neighbor's dog and make it
+        bark but sleep everywhere, but not in my bed. Sniff catnip and act
+        crazy.
+      </p>
+      <p>
+        Throwup on your pillow meow loudly just to annoy owners don't nosh on
+        the birds stare at guinea pigs yet sees bird in air, breaks into cage
+        and attacks creature but knock dish off table head butt cant eat out of
+        my own dish and reward the chosen human with a slow blink. Nya nya nyan.
+        Find something else more interesting snob you for another person yet
+        purr when give birth trip on catnip.
+      </p>
+      <p>
+        Fish i must find my red catnip fishy fish. Mesmerizing birds. Intrigued
+        by the shower curl into a furry donut love blinks and purr purr purr
+        purr yawn.
+      </p>
+    </div>
+  );
+}
