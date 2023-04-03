6823c0e835607a74d51cf60bef83acb9810a89a1
xDamneDx <xdamnedx@gmail.com>
2023-04-03 13:12:11 +0300

    add 404 page & routes

---
+++ src/pages/NotFound.jsx
@@ -0,0 +1,17 @@
+import { Link } from "react-router-dom";
+
+export default function NotFound() {
+  return (
+    <div>
+      <h2>Page Not Found!</h2>
+      <p>
+        Meow meow, i tell my human lay on arms while you're using the keyboard
+        plan your travel but give me some of your food give me some of your food
+        give me some of your food meh, i don't want it.
+      </p>
+      <p>
+        Go to the <Link to="/">Homepage</Link>
+      </p>
+    </div>
+  );
+}
